9e3f247e81750937e9a9b676e00366f5ed95636c
JLegault <jlegault12@gmail.com>
2020-08-04 11:16:55 -0400

    added comments

--- scripts/convert_fmri_mask_to_fs/left_hemisphere/label_to_subject.sh
+++ scripts/convert_fmri_mask_to_fs/left_hemisphere/label_to_subject.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+
+# This is the first step in the pipeline to extract GM measures from individual participants for designated ROIs.
+
+# Makes a log file which outputs any errors. Change the name of the log file everytime you run this.
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>label_to_subject_roi3_log.out 2>&1
 
+# Specify folder where participant freesurfer folders exist
 export SUBJECTS_DIR=/home/qigroup/Documents/projects/blast/derivatives/freesurfer
 
+# Specify folder where mask exists
 cd /home/qigroup/Documents/projects/blast/derivatives/freesurfer
 
+# Specify which ROIs (mlist) and participants (flist) you want to run through.  If you want to change the ROIs, change the contents of the ROI_3.list.txt file.  If you want to change the participants, change the contents of the subject.list.txt file.
 mlist=$(cat /home/qigroup/Documents/projects/blast/derivatives/freesurfer/freesurfer_fsgd_analyses/mask_files/fedorenko_overlap_mask/ROI_3.list.txt)
 flist=$(cat /home/qigroup/Documents/projects/blast/derivatives/freesurfer/freesurfer_fsgd_analyses/mask_files/fedorenko_overlap_mask/subject.list.txt)
 
+# Runs a nested loop where for each ROI you specified, it creates a label file that freesurfer can read.  It then registers that mask to each participant you designated. This will currently only run for the left hemisphere.  If you want to do this for the right hemisphere, change ROI file to the right hemisphere masks and the last flag "--hemi lh" to "--hemi rh".
 for roi in $mlist;
 	do
 	for subject in $flist;
