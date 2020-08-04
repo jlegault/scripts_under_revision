6f3610e3f63fd84185c66c36537ebdc66ada090d
JLegault <jlegault12@gmail.com>
2020-08-04 11:28:01 -0400

    specifies function of log file here

--- scripts/convert_fmri_mask_to_fs/left_hemisphere/extract_GM_from_label.sh
+++ scripts/convert_fmri_mask_to_fs/left_hemisphere/extract_GM_from_label.sh
@@ -2,7 +2,7 @@
 
 # This is the final step in the pipeline to extract GM measures from individual participants for designated ROIs.
 
-# Makes a log file which outputs any errors. Change the name of the log file everytime you run this.
+# Makes a log file which outputs the final GM measures you want for each subject. Change the name of the log file everytime you run this.
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>GM_extracted_roi3_log.out 2>&1
@@ -18,6 +18,7 @@ mlist=$(cat /home/qigroup/Documents/projects/blast/derivatives/freesurfer/freesu
 flist=$(cat /home/qigroup/Documents/projects/blast/derivatives/freesurfer/freesurfer_fsgd_analyses/mask_files/fedorenko_overlap_mask/subject.list.txt)
 
 # Runs a nested loop where for each ROI and participant you specified, it will gather the statistical information like GM measures for each subject's individual subject-specific ROI label file . This will currently only run for the left hemisphere.  If you want to do this for the right hemisphere, change the ROI file to the right hemisphere masks and change the last flag "-b $subject lh" to "-b $subject rh".
+# All of the important information like subject-specific GM measures will be output into the log file you created, e.g. GM_extracted_roi3_log.out.
 for roi in $mlist;
 	do
 	for subject in $flist;
